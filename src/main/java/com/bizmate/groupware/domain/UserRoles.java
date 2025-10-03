f8b5b7d5cb5163b8401e631a1ff54d2a26f093ec
cksdnd1232@gmail.com <cksdnd1232@gmail.com>
2025-10-03 17:43:57 +0900

    groupware 복구작업

---
+++ src/main/java/com/bizmate/groupware/domain/UserRoles.java
@@ -0,0 +1,26 @@
+package com.bizmate.groupware.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_roles")
+public class UserRoles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "role_id")
+    private Long roleId;
+    @Column(name = "emp_id")
+    private Long empId;
+}
