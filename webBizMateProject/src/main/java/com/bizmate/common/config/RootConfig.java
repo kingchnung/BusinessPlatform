784d757c71a8db17e4a8260e63f0183baf927c93
DESKTOP-B3PPDNU\a <isitets88@gmail.com>
2025-10-16 18:56:25 +0900

    부서이동 작업중

--- webBizMateProject/src/main/java/com/bizmate/common/config/RootConfig.java
+++ webBizMateProject/src/main/java/com/bizmate/common/config/RootConfig.java
@@ -1,4 +1,4 @@
-package com.bizmate.project.config;
+package com.bizmate.common.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -14,8 +14,6 @@ public class RootConfig {
         modelMapper.getConfiguration().setFieldMatchingEnabled(true)                             // 필드 매칭을 활성화
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)   // 필드 접근 수준을 PRIVATE으로 설정
                 .setMatchingStrategy(MatchingStrategies.LOOSE);                                 // 매칭 전략을 느슨하게 설정
-
-
         return modelMapper;
     }
 
