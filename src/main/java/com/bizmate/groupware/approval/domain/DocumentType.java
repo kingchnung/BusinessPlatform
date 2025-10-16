6092288dc2c15a73eb60b0756b5e648135b33f46
kingchnung <cksdnd1232@gmail.com>
2025-10-16 17:41:04 +0900

    문서유형 추가

--- src/main/java/com/bizmate/groupware/approval/domain/DocumentType.java
+++ src/main/java/com/bizmate/groupware/approval/domain/DocumentType.java
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DocumentType {
-    REQUEST("REQUEST", "품의서"),        // 품의서
-    RESIGN("RESIGN", "퇴직서"),    // 퇴직서
-    REPORT("REPORT", "보고서"),         // 보고서
-    HR_MOVE("HR_MOVE", "인사발령");     // 인사발령/이동 신청서
+    REQUEST("REQUEST", "품의서"),                     // 일반 품의서
+    PROJECT_PLAN("PROJECT_PLAN", "프로젝트 기획안"),   // 프로젝트 기획안
+    ESTIMATE_PROPOSAL("ESTIMATE_PROPOSAL", "견적서/제안서 발송 품의"),
+    EXPENSE("EXPENSE", "지출결의서"),                 // 지출결의서
+    PURCHASE("PURCHASE", "구매 품의서"),              // 구매 품의서
+    LEAVE("LEAVE", "휴가 신청서"),                    // 휴가신청서
+    RESIGN("RESIGN", "퇴직서"),                       // 사직서
+    HR_MOVE("HR_MOVE", "인사발령");                   // 인사발령서     // 인사발령/이동 신청서
 
     private final String code;
     private final String label;
