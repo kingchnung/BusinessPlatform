38c007411175aec5560519a4c592d0146a7b9a13
jaesung <wkd6344@naver.com>
2025-10-17 10:25:44 +0900

    develop을 위한 커밋

--- webBizMateProject/src/main/java/com/bizmate/project/dto/request/ProjectBoardRequestDTO.java
+++ webBizMateProject/src/main/java/com/bizmate/project/dto/request/ProjectBoardRequestDTO.java
@@ -1,14 +1,21 @@
 package com.bizmate.project.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
 public class ProjectBoardRequestDTO {
 
-    private Long projectBoardId;
-
+    @NotBlank( message = "게시판 명을 입력해주세요")
     private String boardTitle;
 
+    @NotBlank( message = "게시판 타입을 입력해주세요")
     private String boardType;
 
+
+    @NotBlank( message = "프로젝트를 기입 해주세요")
     private Long projectId;
 
-    private Long userId;
+    
+
 }
--- webBizMateProject/src/main/java/com/bizmate/project/service/ProjectBoardService.java
+++ webBizMateProject/src/main/java/com/bizmate/project/service/ProjectBoardService.java
@@ -1,4 +1,7 @@
 package com.bizmate.project.service;
 
+import com.bizmate.project.dto.request.ProjectBoardRequestDTO;
+
 public interface ProjectBoardService {
+    void register(ProjectBoardRequestDTO requestDTO);
 }
